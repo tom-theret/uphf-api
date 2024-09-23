1f57587d786511f3d1d9946004fbe96f8260c21c
Tom Theret <tomnils.theret@gmail.com>
2024-09-23 17:35:23 +0200

    feat(typescript): add import helper

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export * from "./authenticate";
+
+export * from "./api/contactsGet";
+export * from "./api/featuresPost";
+export * from "./api/importantNewsPost";
+export * from "./api/mapGet";
+export * from "./api/restaurantsGet";
+export * from "./api/rssGet";
+export * from "./api/schedulePost";
+export * from "./api/ssoPost";
+export * from "./api/usefulGet";
+export * from "./api/zimbraPost";
